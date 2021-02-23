8c22697b22f1387b238e40c87d9a037e6f73fb15
99003730 <78853960+99003730@users.noreply.github.com>
2021-02-23 17:22:49 +0530

    Add read write functions

--- Project.py
+++ Project.py
@@ -1,8 +1,57 @@
-import re
-file_store=[]
-with open('syslog.txt', 'rt') as file_content:
-    for i in file_content:
-        file_store.append(i)
-file_store=re.split('; |, |\n|\W',str(file_store))
-print(file_store)
+import re                                                 # importing the regex library for performing regex operations
 
+
+def read_file():                                          # creating a function that read the external text file
+    file_store = []                                       # creating a tuple to store the output of file
+    with open('New_text.txt', 'rt') as file_content:        # opening the source file under the name of file_content
+        for characters in file_content:                   # creating a loop to read each characters of file
+            file_store.append(characters)                 # updating the file for each character as the loop progress
+        return file_store                                 # return the updated tuple with the content of file
+
+
+def split_words(file_store):                              # making a function to split whole text into individual words
+    file_split = re.split("[\\W;.,\\n]", str(file_store))
+    ''' The function above splits the whole undivided text into individual words the criteria being if there is a space,
+     new line, full stop or a semicolon separate that text into new index'''
+    return file_split                                     # returns the array with individual words as it's values
+
+
+def word_count(word, file_split):                          # function to count no of occurrences of the given word
+    count = 0                                              # indicating the no of occurrence of word initially it is 0
+    for finding in range(len(file_split)):                 # loop will keep running till the length of array file_split
+        if re.fullmatch(word, file_split[finding], re.M | re.I):
+            ''' The above "if" will only pass if each and every character of the user input matches with the word 
+            in the file_split and this would be repeated for each word in our text file because of the for loop '''
+            count += 1                                     # if the word was found count increases by one
+    return count                                           # return the total no. of occurrences in file
+
+
+def line_printing(word, file_split):
+    line_count = 0
+    k = 1
+    word_file = word+'.txt'
+    new_file=open(word_file,'w')
+    for i in range(len(file_split)):
+        if re.fullmatch("n", file_split[i]):
+            line_count += 1
+            file_split[i] = re.sub('n', '', str(file_split[i]))
+        if re.fullmatch(word, file_split[i], re.M | re.I):
+            previous_word = file_split[i-1]
+            next_word = file_split[i+1]
+            current_word = file_split[i]
+            a="Occurrence number {0} was at Line number {1} : {2} {3} {4} \n".format(k, line_count+1, previous_word,
+                                                                                   current_word, next_word)
+            k+=1
+            print(a)
+            new_file.write(a)
+            ''' new_file=open(word_file, 'a') # opening the source file under the name of file_content
+            new_file.write(a)
+            new_file.close()'''
+    new_file.close()
+
+
+b = input()
+a = read_file()
+c = split_words(a)
+line_printing(b, c)
+z = word_count(b, c)
