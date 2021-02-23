0594e2d683e94810c1287dce697f92bbc410165b
99003730 <78853960+99003730@users.noreply.github.com>
2021-02-23 13:46:32 +0530

    add files

--- Project.py
+++ Project.py
@@ -5,9 +5,4 @@ with open('syslog.txt', 'rt') as file_content:
         file_store.append(i)
 file_store=re.split('; |, |\n|\W',str(file_store))
 print(file_store)
-print(file_store[128])
-'''
-import re
-text = 'The quick brown\nfox jumps*over the lazy dog.'
 
-'''
